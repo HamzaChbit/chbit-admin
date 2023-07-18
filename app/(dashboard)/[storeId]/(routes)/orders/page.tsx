2cdba61ea93293e2f511077cb50b81452406570f
HamzaChbit <hamzachbit@gmail.com>
2023-07-18 18:40:15 +0100

    --ignore-errors

--- app/(dashboard)/[storeId]/(routes)/orders/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/page.tsx
@@ -30,6 +30,8 @@ const OrdersPage = async ({
 
   const formattedOrders: OrderColumn[] = orders.map((item) => ({
     id: item.id,
+    phone: item.phone,
+    address: item.address,
     products: item.orderItems.map((orderItem) => orderItem.product.name).join(', '),
     totalPrice: formatter.format(item.orderItems.reduce((total, item) => {
       return total + Number(item.product.price)
